6e5d4f6b4c3c4ad745c3f7bdaf9612470ab9ad1b
watter46 <w.aoki.14@gmail.com>
2022-11-16 22:58:29 +0900

    addTaskを実装しました

--- backend/resources/js/todo.js
+++ backend/resources/js/todo.js
@@ -8,15 +8,6 @@ const addTask = () => {
     // コマンドを保存
     setTaskCommand();
 
-//     キャレットの後ろに文字が存在しない(先頭)      : checkを先頭の行に配置する
-//     キャレットの後ろに文字が存在する && 
-//         文字とキャレットの間にスペースがない場合  : checkをキャレットがある行の先頭に配置
-//         &&
-//         キャレットが先頭の場合                : bottomに改行
-//         キャレットが間にある(先頭ではない)場合: top、bottomに改行
-
-//         文字とキャレットの間にスペースがある場合　: checkをキャレットがある行から一行飛ばして配置
-
     const textarea_element = document.getElementById("editor_textarea")
     const textarea         = textarea_element.value;
 
@@ -26,91 +17,159 @@ const addTask = () => {
     const after_lines  = textarea.substr(position, length);
     const split_text   = before_lines.split(/\r\n|\n/);
     const last_line    = split_text.slice(-1)[0];
+    const trimed_last  = last_line.trimStart();
 
-    // console.log(position)
-    // console.log(length)
-    // console.log(before_lines)
-    // console.log(after_lines)
-    // console.log(split_text)
-    // console.log(last_line)
-
-    /* キャレットの後ろに文字が存在しない(先頭) */
-    //　キャレットの前が1つのみ split_textのlengthが1
-    if (split_text.length === 1)
+    /* すでにコマンドが存在していた場合 */
+    if (trimed_last.startsWith(getTaskCommand()))
     {
-        console.log("先頭")
-        /* 1行目かつ先頭の場合 */
-        if (!before_lines) {
-            console.log("行先頭")
-            const result = getTaskCommand();
+        const result = before_lines + '\n\n' + getTaskCommand() + after_lines;
 
-            textarea_element.value = result;
-        }
+        textarea_element.value = result;
 
-        /* 1行目かつ先頭ではない場合 */
-        if (before_lines) {
-            console.log("行先頭でない")
-            const result = before_lines + '\n\n' + getTaskCommand();
-
-            textarea_element.value = result;
-        }
-
-        /* キャレットを最後尾に指定 */
+        /* キャレットの位置を指定(2回改行する必要があるため + 2) */
+        const caret_position = position + getTaskCommand().length + 2
         textarea_element.focus()
+        textarea_element.setSelectionRange(caret_position, caret_position)
+        return;
     }
-    else
+
+    /* キャレットの後ろに文字が存在しない場合 */
+    if (!after_lines)
     {
-        console.log("先頭でない")
+        /* キャレットの後ろに文字が存在しない && 1行目 */
+        if (split_text.length === 1)
+        {
+            /* キャレットの後ろに文字が存在しない && 1行目 &&　先頭 */
+            if (!before_lines)
+            {
+                const result = getTaskCommand();
+
+                textarea_element.value = result;
+            }
+
+            /* キャレットの後ろに文字が存在しない && 1行目 &&　先頭ではない */
+            if (before_lines)
+            {
+                const result = before_lines + '\n\n' + getTaskCommand();
+
+                textarea_element.value = result;
+            }
+
+            /* キャレットの位置を指定 */
+            textarea_element.focus()
+        }
+
+        /* キャレットの後ろに文字が存在しない && 1行目ではない */
+        if (!(split_text.length === 1))
+        {
+            const result = before_lines + '\n' + getTaskCommand();
+
+            textarea_element.value = result;
+
+            /* キャレットを最後尾に指定 */
+            textarea_element.focus()
+        }
     }
-    // if (textarea)
-    // {
-    //     console.log("textareaに文字がある時の処理")
 
-    //     // Todo
-    //     // 空文字 + コマンド
-    //     // キャレットがある行を取得
-    //     // キャレットの位置が先頭以外の場合
-    //     /* 
-    //      * キャレットのポジション
-    //      * 
-    //      *
-    //      *
-    //      *
-    //      */
-        
+    /* キャレットの後ろに文字が存在する場合 */
+    if (after_lines)
+    {
+        /* キャレットの後ろに文字が存在する場合 && 1行目 */
+        if (split_text.length === 1)
+        {
+            /* キャレットの後ろに文字が存在する場合 && 1行目 && 先頭 */
+            if (!before_lines)
+            {
+                const result = getTaskCommand() + '\n' + after_lines;
 
-    //     // // before + - [ ] + after
-    //     // split_text.splice(-1, 1, getTaskCommand());
+                textarea_element.value = result;
 
-    //     // const join    = split_text.join()
-    //     // const replace = join.replace(/,/g, '\n')
-    //     // const result  = replace + after_lines
-        
-    //     // document.getElementById("editor_textarea").value = result
+                /* キャレットの位置を指定 */
+                const caret_position = position + getTaskCommand().length
+                textarea_element.focus()
+                textarea_element.setSelectionRange(caret_position, caret_position)
+            }
 
+            /* キャレットの後ろに文字が存在する場合 && 1行目 && 先頭ではない */
+            if (before_lines)
+            {
+                /* キャレットの後ろに文字が存在する場合 && 1行目 && 先頭ではない &&
+                　 キャレットと文字列の間にスペース(半角、全角)が無い場合 */
+                if(/[^( |　)]+$/.test(before_lines))
+                {
+                    const result = getTaskCommand() + before_lines + '\n' + after_lines;
+                    textarea_element.value = result;
 
-    //     // const caret_position = position + getTaskCommand().length
-    //     // textarea_element.focus()
-    //     // textarea_element.setSelectionRange(caret_position, caret_position)
+                    /* キャレットの位置を指定 */
+                    const caret_position = position + getTaskCommand().length
+                    textarea_element.focus()
+                    textarea_element.setSelectionRange(caret_position, caret_position)
+                }
 
-    //     // onPreventEvent();
-    // }
+                /* キャレットの後ろに文字が存在する場合 && 1行目 && 先頭ではない &&
+                　 キャレットと文字列の間にスペース(半角、全角)がある場合 */
+                if(/[( |　)]+$/.test(before_lines))
+                {
+                    const result = before_lines + '\n\n' + getTaskCommand() + '\n' + after_lines;
 
-    // /* textareaに文字がない時の処理 */
-    // if (!textarea)
-    // {
-    //     console.log("textareaに文字がない時の処理")
-    //     textarea_element.value = textarea + getTaskCommand();
-    //     textarea_element.focus()
-    // }
+                    textarea_element.value = result;
+
+                    /* キャレットの位置を指定(2回改行する必要があるため + 2) */
+                    const caret_position = position + getTaskCommand().length + 2
+                    textarea_element.focus()
+                    textarea_element.setSelectionRange(caret_position, caret_position)
+                }
+            }
+        }
+
+        /* キャレットの後ろに文字が存在する場合 && 1行目ではない */
+        if (!(split_text.length === 1))
+        {
+            /* キャレットの後ろに文字が存在する場合 && 1行目ではない &&
+               キャレットと文字列の間にスペース(半角、全角)が無い場合 */
+            if(/[^( |　)]+$/.test(before_lines))
+            {
+                const split_before     = [...split_text]
+                split_before.pop();
+                const new_before_lines = split_before.join().replace(/,/g, '\n');
+                const result = new_before_lines + '\n\n' + getTaskCommand() + last_line + '\n' + after_lines;
+
+                textarea_element.value = result;
+
+                /* キャレットを最後尾に指定(改行するため+1) */
+                const caret_position = position + getTaskCommand().length + 1
+                textarea_element.focus()
+                textarea_element.setSelectionRange(caret_position, caret_position)
+            }
+
+            /* キャレットの後ろに文字が存在する場合 && 1行目ではない &&
+               キャレットと文字列の間にスペース(半角、全角)がある場合 */
+            if(/[( |　)]+$/.test(before_lines))
+            {
+                const result = before_lines + '\n\n' + getTaskCommand() + '\n' + after_lines;
+
+                textarea_element.value = result;
+
+                /* キャレットの位置を指定(2回改行する必要があるため + 2) */
+                const caret_position = position + getTaskCommand().length + 2
+                textarea_element.focus()
+                textarea_element.setSelectionRange(caret_position, caret_position)
+            }
+        }
+    }
 }
+
+/* Ctrl + [ でコマンドを実行する */
+const addTaskCommandExecute = (e) => {
+    if (e.ctrlKey && e.key === "[") {
+        addTask()
+    }
+}
+
 
 const textarea_element = document.getElementById("editor_textarea");
 
 /* Enterが押された時の処理 */
-// textarea_element.addEventListener('keydown', enter_handle, false);
-// const preventEvent    = (e) => e.preventDefault();
-// const onPreventEvent  = (e) => textarea_element.addEventListener('keypress', preventEvent, false );
-// const offPreventEvent = (e) => textarea_element.removeEventListener('keypress', preventEvent, false );
+textarea_element.addEventListener('keydown', addTaskCommandExecute, false);
 
 export {addTask}
